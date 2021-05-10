4db6c2aa527e0ef6c8046908a969563d526ddd05
kyle <mr.th2.90@gmail.com>
2021-05-10 18:48:30 +0700

    update toast

--- src/components/global/Toast.vue
+++ src/components/global/Toast.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
-    <div ref="toast" class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
+    <div ref="toast" class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="d-flex">
         <div class="toast-body">
           {{ message }}
